e256241dc282f017e7f18ce87bdc9308968aa407
yanaanael <33314413+yanaanael@users.noreply.github.com>
2022-03-23 17:50:42 +0200

    Revert "changed the footer"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Yana&Meno</footer>;
+  return <footer>&copy; MyDental</footer>;
 }
 
 export default Footer;
